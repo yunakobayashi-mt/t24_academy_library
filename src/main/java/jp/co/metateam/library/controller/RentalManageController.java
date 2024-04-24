0766127e9c3f906c27c0600896b705c0f6311910
yunakobayashi-mt <yuna.kobayashi@metateam.co.jp>
2024-04-24 11:53:03 +0900

    貸出一覧・登録画面

--- src/main/java/jp/co/metateam/library/controller/RentalManageController.java
+++ src/main/java/jp/co/metateam/library/controller/RentalManageController.java
@@ -5,8 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 //import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.validation.Valid;
+import jp.co.metateam.library.model.Account;
+import jp.co.metateam.library.model.RentalManage;
+import jp.co.metateam.library.model.RentalManageDto;
+import jp.co.metateam.library.model.Stock;
 
 //import org.springframework.web.bind.annotation.ModelAttribute;
 //import org.springframework.web.bind.annotation.PathVariable;
@@ -19,9 +29,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import jp.co.metateam.library.service.AccountService;
 import jp.co.metateam.library.service.RentalManageService;
 import jp.co.metateam.library.service.StockService;
+import jp.co.metateam.library.values.RentalStatus;
 import lombok.extern.log4j.Log4j2;
 
-import jp.co.metateam,library.model.RentalManage;
 
 /**
  * 貸出管理関連クラスß
@@ -53,10 +63,10 @@ public class RentalManageController {
     @GetMapping("/rental/index")
     public String index(Model model) {
         // 貸出管理テーブルから全件取得
-        List<RentalManage> rentalmanageaList = this.rentalManageService.findAll();
+        List<RentalManage> rentalManageList = this.rentalManageService.findAll();
 
         // 貸出一覧画面に渡すデータをmodelに追加
-        model.addAttribute("rentalmanageaList",rentalmanageaList);
+        model.addAttribute("rentalManageList",rentalManageList);
         
         // 貸出一覧画面に遷移
         return "rental/index";
@@ -65,29 +75,38 @@ public class RentalManageController {
     //登録処理
     @GetMapping("/rental/add")
     public String add(Model model){
-        if (!model.containsAttribute("rentalmanageDto")){
-            model.addAttribute("rentalmanageDto",new RentalManageDto());
-        } //もしDtoに変更があったとき、ページ更新時に反映するため
+        List<Account> accountList = accountService.findAll();
+        List<Stock> stockList = stockService.findAll(); //findStockAvailableAllで在庫として利用可能で削除されていないものをとってくる
 
+        if (!model.containsAttribute("rentalManageDto")){
+            model.addAttribute("rentalManageDto",new RentalManageDto());
+        } //もしDtoに変更があったとき、ページ更新時に反映するため
+          //エラーがあって登録画面に戻る際、入力内容が入った状態で返せるように
+
+        model.addAttribute("accounts", accountList);
+        model.addAttribute("stockList", stockList);
+        model.addAttribute("rentalStatus", RentalStatus.values());
+        
         return "rental/add";
 
     }
 
+
     @PostMapping("/rental/add")
-    public String register(@vaild @ModelAttribute RentalMnageDto rentalmanageDto, BindingResult result, RedirectAttributes ra){
+    public String register(@Valid @ModelAttribute RentalManageDto rentalManageDto, BindingResult result, RedirectAttributes ra){
         try {
 
             if(result.hasErrors()){
                 throw new Exception("Validation error.");
             }
         
-        this.rentalManageService.save(rentalmanageDto);
+        this.rentalManageService.save(rentalManageDto);
 
-        return"redirect:/rental/add";
+        return"redirect:/rental/index"; //redirectがないと一覧の画面にデータがないまま戻る
         }catch (Exception e) {
             log.error(e.getMessage());
 
-            ra.addFlashAttribute("rentalmanageDto", rentalmanageDto);
+            ra.addFlashAttribute("rentalManageDto", rentalManageDto);
             //test
 
             return "redirect:/rental/add";
